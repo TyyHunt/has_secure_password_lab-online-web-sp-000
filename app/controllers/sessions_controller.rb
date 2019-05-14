a373913e8bde69d7297f6be2a7551b0815b73a8d
Tyler Hunt <tyhunt04@gmail.com>
2019-05-14 00:20:27 +0000

    Automatically backed up by Learn

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,6 +5,8 @@ class SessionsController < ApplicationController
   def create
     @user = User.find_by(name: params[:name])
     return head(:forbidden) unless @user.authenticate(params[:user][:password])
+    session[:user_id] = @user.id 
+    redirect_to root_path
   end
 
   def destroy
